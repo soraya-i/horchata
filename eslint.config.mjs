0eb1d8805b5f693ae1d0e31b6c1a13a4a88be757
Omar <ohyjek+1@gmail.com>
2025-08-09 15:56:10 -0400

    pin versions, add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
+import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-plugin-prettier/recommended';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends('next/core-web-vitals'), // All essentials
+  prettier,
+  {
+    rules: {
+      'prettier/prettier': ['warn', {}, { usePrettierrc: true }], // Only if using Prettier
+    },
+  },
 ];
-
-export default eslintConfig;
